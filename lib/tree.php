fbe8f98e3ebb1df84bd9835e9717a7f25f48dbfd
Martins Lazdans <marrtins@dqdp.net>
2021-09-09 23:38:09 +0300

    Separate CountryRangeTreeNode

--- lib/tree.php
+++ lib/tree.php
@@ -1,20 +1,5 @@
 <?php
 
-class CountryRangeTreeNode {
-	var $min;
-	var $max;
-	var $key;
-	var $left;
-	var $right;
-
-	function __construct(&$data, $key){
-		$this->key = $key;
-		$this->min = $data[$key]->start;
-		$this->max = $data[$key]->end;
-		$this->left = $this->right = NULL;
-	}
-}
-
 function build_tree(&$data, $floor, $ceil){
 	if($floor > $ceil){
 		return null;
