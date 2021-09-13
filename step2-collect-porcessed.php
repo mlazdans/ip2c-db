4d40b03cbcd3214474ea226ef8f237a201d78865
Martins Lazdans <marrtins@dqdp.net>
2021-09-14 01:42:49 +0300

    Optional parameters

--- step2-collect-porcessed.php
+++ step2-collect-porcessed.php
@@ -5,8 +5,43 @@ declare(strict_types = 1);
 require_once('boot.php');
 require_once('console.php');
 
-$countries = array();
-foreach(glob($CONFIG['whoisdata_root'].DIRECTORY_SEPARATOR."*.processed") as $database){
+$O = getopt("p:r:h");
+
+function usage(){
+	global $argv;
+
+	print "\nUsage: $argv[0] -p <whoisdata_root> -r <output_folder> [-h]\n";
+	print "\n";
+	print "\t-p .processed WHOIS / delegated databases folder\n";
+	print "\t-r Output folder for ip2country files\n";
+	print "\t-h Help\n";
+
+	exit(1);
+}
+
+if(empty($O['r']) || empty($O['p']) || isset($O['h']))
+	usage();
+
+$WHOIS_ROOT = realpath($O['p'].DIRECTORY_SEPARATOR);
+if(!$WHOIS_ROOT || !is_readable($WHOIS_ROOT)){
+	print "WHOIS folder not readable ($WHOIS_ROOT)\n";
+	exit(1);
+}
+
+$OUTPUT_ROOT = realpath($O['r'].DIRECTORY_SEPARATOR);
+if(!$OUTPUT_ROOT || !is_writable($OUTPUT_ROOT)){
+	print "Output folder not writable ($OUTPUT_ROOT)\n";
+	exit(1);
+}
+
+$DATABASES = glob($WHOIS_ROOT.DIRECTORY_SEPARATOR."*.processed");
+if(!count($DATABASES)){
+	print "No .processed databases found\n";
+	exit(1);
+}
+
+$countries = [];
+foreach($DATABASES as $database){
 	print "Loading: $database...";
 	$f = fopen($database, 'r');
 	while(!feof($f)){
@@ -18,7 +53,7 @@ foreach(glob($CONFIG['whoisdata_root'].DIRECTORY_SEPARATOR."*.processed") as $da
 	print "DONE\n";
 }
 
-$output_dir = $CONFIG['tempin_root'].DIRECTORY_SEPARATOR;
+$output_dir = $OUTPUT_ROOT.DIRECTORY_SEPARATOR;
 delfiles("$output_dir*.db");
 
 print "Saving: $output_dir*.db...";
